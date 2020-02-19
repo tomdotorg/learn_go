8f2caf86c6c28af146c0f02811ee9431bef134ed
Tom Mitchell <tom@tom.org>
2020-02-19 06:11:12 -0500

    just learnin

--- morestrings/go.mod
+++ morestrings/go.mod
@@ -1,3 +1,5 @@
 module github.com/tomdotorg/learn_go/morestrings
 
 go 1.13
+
+require golang.org/x/tour v0.0.0-20200201212631-8f38c9a8d074 // indirect
---
+++ tutorialspoint/hello.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/* function returning the max between two numbers */
+func max(num1, num2 int) int {
+	/* local variable declaration */
+	var result int
+
+	if num1 > num2 {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
+}
+
+func swap(a string, b string) (string, string) {
+	return b, a
+}
+
+func main() {
+
+	var foo, bar int32
+	var s = "string"
+	foo = 5
+	fmt.Println("foo", foo)
+	fmt.Println(bar)
+	fmt.Println(&s)
+	//	fmt.Println(*s)
+	/* This is my first sample program. */
+	_foo := 5
+	_foo += 5
+	fmt.Println("Hello, World!")
+	fmt.Println(max(2, 3))
+	fmt.Println(swap("second", "first"))
+	fmt.Println("I am in Go Programming World!")
+	for i := 0; i < 3; i++ {
+		fmt.Printf("i: %d\n", i)
+	}
+}
